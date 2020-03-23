1382f1ccece089ddd9b4defcca7fed8c75041d80
Yogeshwargundale <yogeshwargundale@gmail.com>
2020-03-23 11:12:04 +0530

    minor code format

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Header from '../components/Header'
-import { Row, Col, Table } from 'antd';
+import { Row, Col, Table ,Timeline} from 'antd';
 import './style.css'
 const BASE_URL='https://93870v1pgk.execute-api.ap-south-1.amazonaws.com/latest/shipments';
 const axios = require("axios");
@@ -56,7 +56,6 @@ class Dashboard extends Component{
       
     }
     componentDidMount(){
-        console.log('component did mount')
         axios({
             method: 'post',
             url: `${BASE_URL}/yogeshwar`,
@@ -65,7 +64,6 @@ class Dashboard extends Component{
             },
             headers: {'AUTHORIZATION': 'Bearer tTU3gFVUdP'}
           }) .then((response) => {
-            console.log(response);
             const dataobj = [];
             
             const result = response.data.data.forEach(element => {
@@ -100,11 +98,9 @@ class Dashboard extends Component{
               }
               dataobj.push({'awbno':element.awbno,'carrier':element.carrier,'from':element.from,'to':element.to,
               'carrier':element.carrier,'pickup_date':element.pickup_date,'etd':element.extra_fields!==undefined?element.extra_fields.expected_delivery_date:'',
-              'current_status':element.current_status
+              'current_status':element.current_status,'scan':element.scan
             })
           })
-            console.log(dataobj);
-            console.log('OOD count',this.state.OOD)
             this.setState({shipment_data:dataobj})
           })
           .catch((error) => {
@@ -145,7 +141,6 @@ class Dashboard extends Component{
                 key: 'pickup_date',
               },
               {
-                // response.data.data[0].extra_fields.expected_delivery_date
                 title: 'ETD',
                 dataIndex: 'etd',
                 key: 'etd',
@@ -160,17 +155,6 @@ class Dashboard extends Component{
         return (
             <div style={{backgroundColor:"rgb(255, 255, 255)"}}>
                 <Header />
-
-                {/* <Row> */}
-                {/* <Col offset="6">
-                  <Col><div className="count">{this.state.DEL}</div></Col>
-                  <Col><div>{this.state.OOD}</div></Col>
-                  <Col><div>{this.state.INT}</div></Col>
-                  <Col><div>{this.state.DEX}</div></Col>
-                  <Col><div>{this.state.NFI}</div></Col>
-                </Col>
-                <Col offset="8"></Col> */}
-                {/* <Col span="12"> */}
                   <div className="count-container">
                   <div onClick={() =>this.sortShipment('DEL')}><span >DEL</span><br/>{this.state.DEL}</div>
                   <div onClick={() =>this.sortShipment('OOD')}>OOD <br/>{this.state.OOD}</div>
@@ -178,10 +162,10 @@ class Dashboard extends Component{
                   <div onClick={() =>this.sortShipment('DEX')}>DEX <br/>{this.state.DEX}</div>
                   <div onClick={() =>this.sortShipment('NFI')}>NFI <br/>{this.state.NFI}</div>
                   </div>
-                {/* </Col> */}
-                {/* </Row> */}
                 <Row>
-                    <Col offset="1" span="7"></Col>
+                    <Col offset="1" span="7">
+                   
+                    </Col>
                     <Col offset="1" />
                     <Col span="15">
                     <Table 
